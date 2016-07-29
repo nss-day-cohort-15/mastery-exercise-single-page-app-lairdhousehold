3a132fcac9878ef436813159606e5eba5767e11d
Jammy Laird <jammylaird@yahoo.com>
2016-07-29 15:26:31 -0500

    worked on xhr

---
+++ cars.js
@@ -0,0 +1,15 @@
+var cars =(function (){
+  var inventory =[];
+
+  return {
+    getInventory: function () {
+    },
+    loadInventory : function (cb) {
+      var InventoryLoader = XMLHttpRequest();
+
+      inventoryLoader.addEventListner("load", function(){
+
+      });
+    }
+  };
+})();
