25b19e545e7cf93506a78022d5d886e7df5b9d9f
Jammy Laird <jammylaird@yahoo.com>
2016-08-02 14:54:32 -0500

    updating js still working through it

--- quiz.js
+++ quiz.js
@@ -2,16 +2,22 @@
 function populatePage (inventory) {
   // state the needed variables
   var carList = document.querySelector(".container")
-
   var output ="";
   // Loop over the inventory and populate the page
-for (var i=0; i < inventory.cars.length; i++)
-  var currentCar = inventory[i];
-  CarList.innerHTML+= '<div>${currentCar.item}</div>'
-  // Now that the DOM is loaded, establish all the event listeners needed
-  // CarLot.activateEvents();
+  for (var i=0; i < inventory.cars.length; i++){
+    if (inventory[i].purchased === false) {
+      var purchased = <div class="for sale">For sale!</div>
+} else{
+      var purchased = <div class="sold">Sorry Sold!</div>
 }
-console.log('I ran')
+
+
+    var currentCar = inventory[i];
+    CarList.innerHTML+= '<div>${currentCar.item}</div>'
+    // Now that the DOM is loaded, establish all the event listeners needed
+    // CarLot.activateEvents();
+  }
+  console.log('I ran')
 // Load the inventory and send a callback function to be
 // invoked after the process is complete
 // CarLot.loadInventory();
